f7cd6764fb95fd8e62c231b01d02838d225129bd
nparfyonov <nikita.parfyonov@hf-solution.com>
2023-08-01 00:14:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = bracketsConfig.map(a => a.join(''))
+  let temp = ''
+  while (str.length != temp.length) {
+    temp = str
+    pairs.forEach(pair => {
+      str = str.replace(pair, '')
+    })
+  }
+  return str.length === 0
 }
+
+/* module.exports = function check(str, bracketsConfig) {
+  const regex = new RegExp(bracketsConfig.map(a => '\\' + a.join('\\')).join('|'))
+  let temp = ''
+  while (str.length != temp.length) {
+    temp = str
+    str = str.replace(regex, '')
+  }
+  return str.length === 0
+} */
